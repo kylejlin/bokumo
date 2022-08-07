904563e3a9e7cb63fe37d56d8daf3854bd2a985b
Kyle Lin <kylejlin.us@gmail.com>
2022-08-07 16:09:25 -0700

    Get audio stream

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { App } from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+navigator.mediaDevices
+  .getUserMedia({ video: false, audio: true })
+  .then((stream) => {
+    root.render(
+      <React.StrictMode>
+        <App stream={stream} />
+      </React.StrictMode>
+    );
+  })
+  .catch((error) => {
+    root.render(
+      <React.StrictMode>
+        <p>
+          Could not get audio stream. Please make sure you have a microphone
+          enabled.
+        </p>
+      </React.StrictMode>
+    );
+    window.alert(
+      "Could not get audio stream. Please make sure you have a microphone enabled."
+    );
+    console.log(
+      "Could not get audio stream. Please make sure you have a microphone enabled.",
+      { error }
+    );
+  });
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
